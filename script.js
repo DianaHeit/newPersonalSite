b83c4833c086ab600d17363ed7f88ad0a6b05841
DianaHeit <diana.heit@gmx.de>
2019-10-27 13:41:14 +0100

    update

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
+// jQuery toggle menu
 $("button").click(function() {
   $("header").animate({
     height: 'toggle'
   });
+});
+
+// JavaScript toggle background color style
+var button = document.querySelector("button");
+var body = document.querySelector("body");
+
+button.addEventListener("click", function(){
+  body.classList.toggle("toggle-style");
 });
